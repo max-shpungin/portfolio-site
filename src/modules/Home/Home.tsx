2c69cd1ff35a67aa36694fbd9a2d25c11614c343
Max Shpungin <maxsdance@gmail.com>
2024-02-19 06:15:45 -0800

    moved icon panel to its own component file

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,3 +1,5 @@
+import IconPanel from '../IconPanel/IconPanel';
+
 import profilePic from '../../assets/profile-pic.png';
 import pinkCat from '../../assets/pink_cat.jpg';
 import orangeCafe from '../../assets/orange_cafe.jpg';
@@ -23,29 +25,7 @@ function Home() {
 
             </nav>
 
-
-
-            <div className="IconPanel row my-5">
-                <div className="icons d-flex mx-auto justify-content-center">
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original-wordmark.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" />
-                </div>
-                <div className="icons d-flex mx-auto justify-content-center">
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original-wordmark.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original-wordmark.svg" />
-                </div>
-                <div className="icons d-flex mx-auto justify-content-center">
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original-wordmark.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original-wordmark.svg" />
-                    <img className='col-2' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg" />
-                </div>
-            </div>
+            <IconPanel/>
 
 
             <div className="row">
