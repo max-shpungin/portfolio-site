d66f7181851df53a9a2436b784c871e7d4d37247
Max Shpungin <maxsconsulting@gmail.com>
2024-03-14 17:45:21 -0700

    change order of featured work

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -18,10 +18,10 @@ function Home() {
 
             <div className="intro col-6 mx-auto m-5">
                 <h1 className="h1 fs-2"> Hey! I'm <span className='name'>Max Shpungin</span>, a software engineer with a background in digital analytics.</h1>
-                <br/>
+                <br />
                 <p className='fw-semibold fs-3'> I love building <span className='cool-things'>cool things, my cat, and my
-                        <span className='wife'> wife</span>
-                    </span>.
+                    <span className='wife'> wife</span>
+                </span>.
                     <br /><br />
                     <span className='fs-6'>
                         not necessarily in that order.
@@ -36,20 +36,8 @@ function Home() {
 
                 <div className="container-fluid projects">
                     <div className="row row-cols-1 row-cols-md-2 g-3">
-                        <div className="col">
 
-                            <div className="card shadow-sm rounded-5 border-2">
-                                <img src={warbler} className="card-img-top rounded-top-5" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">Warbler</h5>
-                                    <p className="card-text">A twitter clone except with warbles :D.</p>
-                                    <a className="btn btn-outline-dark me-1" href="https://github.com/max-shpungin/warbler/">Github</a>
-                                    <a className="btn btn-outline-primary" href="https://max-shpungin-warbler.onrender.com/">View on Render</a>
-                                </div>
-                            </div>
-                        </div>
                         <div className="col">
-
                             <div className="card shadow-sm rounded-5 border-2">
                                 <img src={friender} className="card-img-top rounded-top-5" alt="..." />
                                 <div className="card-body">
@@ -60,6 +48,19 @@ function Home() {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="col">
+                            <div className="card shadow-sm rounded-5 border-2">
+                                <img src={warbler} className="card-img-top rounded-top-5" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Warbler</h5>
+                                    <p className="card-text">A twitter clone except with warbles :D.</p>
+                                    <a className="btn btn-outline-dark me-1" href="https://github.com/max-shpungin/warbler/">Github</a>
+                                    <a className="btn btn-outline-primary" href="https://max-shpungin-warbler.onrender.com/">View on Render</a>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
