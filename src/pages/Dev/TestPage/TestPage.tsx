6ef762f697d306a662ad5c0a9f17ad5cec62ebd1
Max Shpungin <maxsconsulting@gmail.com>
2024-10-03 19:21:03 -0400

    updated test page to remove old conflicts

--- src/pages/Dev/TestPage/TestPage.tsx
+++ src/pages/Dev/TestPage/TestPage.tsx
@@ -1,30 +1,9 @@
-import { useState, MouseEvent } from "react";
-
-import ModalContactForm from "../../../modules/Modals/ModalForm/ModalForm";
-import ContactForm from "../../../modules/Forms/ContactForm";
-
 function TestPage() {
 
-    const[showModal, setShowModal] = useState(false);
-
-    const handleOpen = (event: MouseEvent) => {
-        event.preventDefault();
-        setShowModal(true);
-    };
-
-    const handleClose = (event:MouseEvent) => {
-        event.preventDefault();
-        setShowModal(false)
-    }
 
     return (
         <div className="TestPage">
-            <div className="test-container">
-            <button className="what" onClick={handleOpen}>
-                shomethemoney
-            </button>
-                <ModalContactForm form={<ContactForm/>} showModal={showModal} handleClose={handleClose} />
-            </div>
+            Well - this is awkward.
         </div>
     )
 }
