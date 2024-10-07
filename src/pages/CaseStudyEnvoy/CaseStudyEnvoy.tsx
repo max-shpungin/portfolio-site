dcbd92b2d4775512ee0e27cb33b1914401aebc9b
Max Shpungin <maxsconsulting@gmail.com>
2024-10-07 14:34:32 -0700

    fixed css HTML structure issues that should address display on iphones

--- src/pages/CaseStudyEnvoy/CaseStudyEnvoy.tsx
+++ src/pages/CaseStudyEnvoy/CaseStudyEnvoy.tsx
@@ -73,10 +73,8 @@ function Outtro() {
                         Get in touch!
                     </button>
                     <br></br>
-                    <a href="#top">
-                        <button className="outtro-button-top">
+                    <a href="#top" className="outtro-button-top">
                             <p>Take me to the top!</p>
-                        </button>
                     </a>
                     <ModalForm
                         form={<ContactForm handleSuccess={handleSuccess} name="contact-case-study" />}
